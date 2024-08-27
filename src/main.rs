cc46adc1b4e5b7e3c1a60e647c0552ba5e577144
Jeremi Do Dinh <jeremi.dodinh@gmail.com>
2024-08-27 22:36:22 +0200

    Implement torrent info decoding

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use serde_json::{Value, Number, Map};
+use core::panic;
+use std::str;
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::fmt::Write;
 
 // Available if you need it!
 // use serde_bencode
@@ -7,49 +13,49 @@ use std::env;
 #[cfg(test)]
 mod tests;
 
-fn decode_bencoded_value(encoded_value: &str) -> Value {
+fn decode_bencoded_value(encoded_value: Vec<u8>) -> Value {
     let (decoded_value, _) = decode(encoded_value).unwrap();
     decoded_value
 }
 
 #[allow(dead_code)]
-fn decode(encoded_value: &str) -> Result<(Value, usize), &'static str> {
+fn decode(encoded_value: Vec<u8>) -> Result<(Value, usize), &'static str> {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+    if encoded_value.iter().next().unwrap().is_ascii_digit() {
         // Example: "5:hello" -> "hello"
         return decode_str(encoded_value);
-    } else if encoded_value.chars().next().unwrap() == 'i' {
+    } else if encoded_value.iter().next().unwrap().to_owned() == 'i' as u8 {
         return decode_int(encoded_value);
-    } else if encoded_value.chars().next().unwrap() == 'l' {
+    } else if encoded_value.iter().next().unwrap().to_owned() == 'l' as u8 {
         return decode_list(encoded_value);
-    } else if encoded_value.chars().next().unwrap() == 'd' {
+    } else if encoded_value.iter().next().unwrap().to_owned() == 'd' as u8 {
         return decode_dict(encoded_value);
     } else {
         Err("invalid value to decode")
     }
 }
 
-fn decode_str(encoded_value: &str) -> Result<(Value, usize), &'static str> {
-    let colon_index = encoded_value.find(':').unwrap();
-    let number_string = &encoded_value[..colon_index];
+fn decode_str(encoded_value: Vec<u8>) -> Result<(Value, usize), &'static str> {
+    let colon_index = encoded_value.iter().position(|c| (':' as u8).eq(c)).unwrap();
+    let number_string = str::from_utf8(&encoded_value[..colon_index]).unwrap();
     let number = number_string.parse::<i64>().unwrap();
-    let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-    return Ok((Value::String(string.to_string()), colon_index + 1 + number as usize));
+    let string = String::from_utf8_lossy(&encoded_value[colon_index + 1..colon_index + 1 + number as usize]);
+    return Ok((Value::String(string.into_owned()), colon_index + 1 + number as usize));
 }
 
-fn decode_int(encoded_value: &str) -> Result<(Value, usize), &'static str> {
-    let end_index = encoded_value.find('e').unwrap();
-    let number_string = &encoded_value[1..end_index];
+fn decode_int(encoded_value: Vec<u8>) -> Result<(Value, usize), &'static str> {
+    let end_index = encoded_value.iter().position(|c| ('e' as u8).eq(c)).unwrap();
+    let number_string = String::from_utf8_lossy(&encoded_value[1..end_index]);
     let number = number_string.parse::<i64>().unwrap();
     return Ok((Value::Number(Number::from(number)), end_index + 1));
 }
 
-fn decode_list(encoded_value: &str) -> Result<(Value, usize), &'static str> {
+fn decode_list(encoded_value: Vec<u8>) -> Result<(Value, usize), &'static str> {
     let mut vector: Vec<Value> = Vec::new();
     let mut bytes_read: usize = 1;
     let mut string_slice = &encoded_value[1..];
-    while string_slice.chars().next().unwrap() != 'e' {
-        let (value, size) = decode(string_slice).unwrap();
+    while string_slice.iter().next().unwrap().to_owned() != 'e' as u8 {
+        let (value, size) = decode(string_slice.to_vec()).unwrap();
         vector.push(value);
         bytes_read += size;
         string_slice = &encoded_value[bytes_read..]
@@ -57,21 +63,45 @@ fn decode_list(encoded_value: &str) -> Result<(Value, usize), &'static str> {
     return Ok((Value::Array(vector), bytes_read + 1));
 }
 
-fn decode_dict(encoded_value: &str) -> Result<(Value, usize), &'static str> {
+fn decode_dict(encoded_value: Vec<u8>) -> Result<(Value, usize), &'static str> {
     let mut bytes_read: usize = 1;
     let mut map: Map<String, Value> = Map::new();
     let mut string_slice = &encoded_value[1..];
-    while string_slice.chars().next().unwrap() != 'e' {
-        let (key, offset) = decode_str(string_slice).unwrap();
+    while string_slice.iter().next().unwrap().to_owned() != 'e' as u8 {
+        let (key, offset) = decode_str(string_slice.to_vec()).unwrap();
         bytes_read += offset;
         string_slice = &string_slice[offset..];
-        let (value, offset) = decode(string_slice).unwrap();
+        let (value, offset) = decode(string_slice.to_vec()).unwrap();
         bytes_read += offset;
         map.insert(key.as_str().unwrap().to_owned(), value);
         string_slice = &string_slice[offset..];
     }
     return Ok((Value::Object(map), bytes_read + 1));
 }
+
+fn fmt_torrent_details(file_contents: Vec<u8>) -> String {
+    let mut buffer = String::new();
+    // for (i, e) in file_contents.iter().enumerate() {
+    //     println!("{}: {:?}", i, e);
+    // }
+    let map: Value = match decode_dict(file_contents) {
+        Ok((map, _)) => map,
+        Err(why) => panic!("error decoding map: {}", why),
+    };
+    match map.get("announce") { 
+        Some(url) => writeln!(buffer, "Tracker URL: {}", url.as_str().unwrap()).unwrap(),
+        None => panic!("invalid torrent file: No tracker URL found"),
+    }
+    match map.get("info") {
+        Some(info_map) => match info_map.get("length") {
+            Some(length) => writeln!(buffer, "Length: {}", length).unwrap(),
+            None => panic!("invalid torrent file: No torrent length found"),
+        },
+        None => panic!("invalid torrent file: No tracker URL found"),
+    };
+    return buffer;
+}
+
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
@@ -83,9 +113,21 @@ fn main() {
         // println!("Logs from your program will appear here!");
 
         // Uncomment this block to pass the first stage
-        let encoded_value = &args[2];
+        let encoded_value: Vec<u8> = Vec::from(args[2].clone());
         let decoded_value = decode_bencoded_value(encoded_value);
         println!("{}", decoded_value.to_string());
+    } else if command == "info" {
+        let file_path = Path::new(&args[2]);
+        let mut file = match File::open(&file_path) {
+            Err(why) => {println!("couldn't open file: {}", why); self::panic!();},
+            Ok(file) => file,
+        };
+        let mut file_contents: Vec<u8> = Vec::new();
+        match file.read_to_end(&mut file_contents) {
+            Err(why) => {println!("failed to read file contents: {}", why); self::panic!()},
+            Ok(_) => (),
+        }
+        println!("{}", fmt_torrent_details(file_contents));
     } else {
         println!("unknown command: {}", args[1])
     }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -2,30 +2,30 @@ use super::*;
 
 #[test]
 fn test_string_decode() {
-    let string = "5:hello";
+    let string = Vec::from("5:hello");
     let result = decode_bencoded_value(string);
     assert_eq!(result, Value::String("hello".to_owned()));
 
-    let long_string = "15:hellohellohello";
+    let long_string = Vec::from("15:hellohellohello");
     let result = decode_bencoded_value(long_string);
     assert_eq!(result, Value::String("hellohellohello".to_owned()));
 }
 
 #[test]
 fn test_integer_decode() {
-    let integer_string = "i127435439e";
+    let integer_string = Vec::from("i127435439e");
     let result = decode_bencoded_value(integer_string);
     assert_eq!(result, serde_json::Value::Number(serde_json::Number::from(127435439 as i64)));
 
-    let integer_string = "i-9e";
+    let integer_string = Vec::from("i-9e");
     let result = decode_bencoded_value(integer_string);
     assert_eq!(result, serde_json::Value::Number(serde_json::Number::from(-9 as i64)));
 
-    let integer_string = "i0e";
+    let integer_string = Vec::from("i0e");
     let result = decode_bencoded_value(integer_string);
     assert_eq!(result, serde_json::Value::Number(serde_json::Number::from(0 as i64)));
 
-    let integer_string = "i-0e";
+    let integer_string = Vec::from("i-0e");
     let result = decode_bencoded_value(integer_string);
     assert_eq!(result, serde_json::Value::Number(serde_json::Number::from(0 as i64)));
 }
@@ -33,26 +33,26 @@ fn test_integer_decode() {
 #[test]
 #[should_panic]
 fn test_integer_decode_invalid() {
-    let integer_string = "ie";
+    let integer_string = Vec::from("ie");
     let result = decode_bencoded_value(integer_string);
     assert_eq!(result, serde_json::Value::Number(serde_json::Number::from(0 as i64)));
 }
 
 #[test]
 fn test_list_decode() {
-    let list_string = "l5:helloi52ee";
+    let list_string = Vec::from("l5:helloi52ee");
     let result = decode_bencoded_value(list_string);
     assert_eq!(result, Value::Array(Vec::from([Value::String("hello".to_owned()), Value::Number(Number::from(52))])));
 
-    let list_string = "l15:hellohellohelloi52ee";
+    let list_string = Vec::from("l15:hellohellohelloi52ee");
     let result = decode_bencoded_value(list_string);
     assert_eq!(result, Value::Array(Vec::from([Value::String("hellohellohello".to_owned()), Value::Number(Number::from(52))])));
 
-    let list_string = "l5:helloi52el5:helloi52eee";
+    let list_string = Vec::from("l5:helloi52el5:helloi52eee");
     let result = decode_bencoded_value(list_string);
     assert_eq!(result, Value::Array(Vec::from([Value::String("hello".to_owned()), Value::Number(Number::from(52)), Value::Array(Vec::from([Value::String("hello".to_owned()), Value::Number(Number::from(52))]))])));
 
-    let list_string = "llllll5:helloeeeeee";
+    let list_string = Vec::from("llllll5:helloeeeeee");
     let result = decode_bencoded_value(list_string);
     let expected = Value::Array(Vec::from([Value::Array(Vec::from([Value::Array(Vec::from([Value::Array(Vec::from([Value::Array(Vec::from([Value::Array(Vec::from([Value::String("hello".to_owned())]))]))]))]))]))]));
     assert_eq!(result, expected);
@@ -60,7 +60,7 @@ fn test_list_decode() {
 
 #[test]
 fn test_dict_decode() {
-    let dict_string = "d3:foo3:bar5:helloi52ee";
+    let dict_string = Vec::from("d3:foo3:bar5:helloi52ee");
     let result = decode_bencoded_value(dict_string);
     let mut expected = serde_json::Map::new();
     expected.insert("foo".to_owned(), Value::String("bar".to_owned()));
@@ -69,4 +69,20 @@ fn test_dict_decode() {
     assert_eq!(result, expected);
 }
 
-
+#[test]
+fn test_torrent_file_parse() {
+    let file_path = Path::new("src/tests/resources/test.torrent");
+    let mut file = match File::open(&file_path) {
+        Err(why) => {println!("couldn't open file: {}", why); self::panic!();},
+        Ok(file) => file,
+    };
+    let mut file_contents: Vec<u8> = Vec::new();
+    match file.read_to_end(&mut file_contents) {
+        Err(why) => {println!("failed to read file contents: {}", why); self::panic!()},
+        Ok(_) => (),
+    }
+    println!("{:?}", file_contents);
+    let details = fmt_torrent_details(file_contents);
+    let expected = "Tracker URL: http://example.com\nLength: 1000\n";
+    assert_eq!(details, expected);
+}
